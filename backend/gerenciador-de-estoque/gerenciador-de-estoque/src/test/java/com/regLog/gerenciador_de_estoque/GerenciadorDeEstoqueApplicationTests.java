0eeec559ff4c836976d61a39233ec39ec7b828d3
AyrtonF <ayrtonleonardo14@gmail.com>
2025-02-07 14:44:23 -0300

    estruturando projeto

---
+++ backend/gerenciador-de-estoque/gerenciador-de-estoque/src/test/java/com/regLog/gerenciador_de_estoque/GerenciadorDeEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regLog.gerenciador_de_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
